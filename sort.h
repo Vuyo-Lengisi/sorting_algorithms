5f3b65b85adc7781c8e6464894410e45c6e40e3a
Vuyo-Lengisi <lvuyo1@gmail.com>
2024-02-25 03:50:54 +0200

    Sorting algorithms & Big O up to date

--- sort.h
+++ sort.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* Comparison direction macros for bitonic sort */
 #define UP 0
 #define DOWN 1
 
@@ -32,10 +33,23 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-
+/* Printing helper functions */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
 /* Sorting algoritms */
 void bubble_sort(int *array, size_t size);
-void insertion_sort_list(listint_t **list);#ifndef
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+
+#endif /* SORT_H */
+
